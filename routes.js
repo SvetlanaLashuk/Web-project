4f050a30318e19abbccbb6b820fb2fbddc689835
Svetlana Lashuk <svetlana.lashuk311@gmail.com>
2018-05-09 17:45:18 +0300

    Add function for adding rss channels

--- routes.js
+++ routes.js
@@ -15,6 +15,28 @@ module.exports = (app, db, parser) => {
         );
     });
 
+    app.put("/feed", (req, res) => {
+        db.collection("feeds").findOneAndUpdate(
+            {ip: req.ip},
+            {
+                $push: {
+                    feeds: {
+                        title: req.body.title,
+                        url: req.body.url
+                    }
+                }
+            },
+            {upsert: true, returnOriginal: false},
+            (err, result) => {
+                if (err) {
+                    res.send({error: err});
+                } else {
+                    res.send(result.value);
+                }
+            }
+        );
+    });
+
     app.post("/feed", (req, res) => {
         let promise = new Promise(resolve =>
             resolve(parser.parseURL(req.body.url))
